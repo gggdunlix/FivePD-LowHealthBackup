a14fe219074a051abca85783678734da9ceb2921
BuildTools <unconfigured@null.spigotmc.org>
2022-01-13 15:06:06 -0500

    Add project files.

---
+++ LowHealthBackup/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using FivePD.API;
+using FivePD.API.Utils;
+using static FivePD.API.Callout;
+
+
+namespace LowHealthBackup
+{
+    public class LowHealth : FivePD.API.Plugin
+    {
+
+        internal LowHealth()
+        {
+            int health = CitizenFX.Core.Native.API.GetEntityHealth(Game.PlayerPed.NetworkId);
+            if (health < 10)
+            {
+                FivePD.API.Utilities.RequestBackup(Utilities.Backups.Code99);
+                
+            }
+        }
+
+    }
+}
